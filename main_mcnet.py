e87498b4864dc5fd59d4abdb4e37d433d5fd3c44
quackqing <38725352+quackqing@users.noreply.github.com>
2021-02-22 11:14:22 +0800

    Update main_mcnet.py

--- main_mcnet.py
+++ main_mcnet.py
@@ -8,18 +8,16 @@ from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import keras.backend as kb
 
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['KERAS_BACKEND'] = 'tensorflow'
-
 
 '''
     文献中MCNet网络结构识别
     数据集：2018.01a DATASETS
     包含24中调制方式，每种调制方式有[-20:2:30]共25个SNRs，每个snr有4096条数据，每条数据I/Q两路数据，每路数据有1024个点
     数据集的大小为  2555904*2*1024
-    
-    
+
+
     MCNet网络结构：
             out-dim
     Input: 2*1024*1
@@ -40,12 +38,12 @@ os.environ['KERAS_BACKEND'] = 'tensorflow'
     M_Bk:  2*8*256
     concat:
     pool:
-    
+
     dense:
     softmax:
-    
+
     classification                     
-    
+
 '''
 
 # 分割数据集
@@ -63,7 +61,7 @@ for itr in range(9):
     f.close()
 
     n_examples = x_data.shape[0]
-    train_index = np.random.choice(range(0, n_examples), size=int(0.7*n_examples), replace=False)
+    train_index = np.random.choice(range(0, n_examples), size=int(0.7 * n_examples), replace=False)
     test_index = list(set(range(0, n_examples)) - set(train_index))
 
     if itr == 0:
@@ -89,18 +87,14 @@ print('测试集Y维度：', y_test.shape)
 print('测试集Z维度：', z_test.shape)
 
 
-
 # pre_block模块
 def pre_block(xm, mcSeq, pool_size):
-
     print('进入pre-block模块')
     base = xm
-    xm0 = Conv2D(32, (1, 3), padding='same', name=mcSeq + "_pre_block model_conv00", kernel_initializer='glorot_normal', data_format='channels_first')(base)
+    xm0 = Conv2D(32, (3, 1), padding='same', name=mcSeq + "_pre_block_model_conv00", kernel_initializer='glorot_normal', data_format='channels_first')(base)
     xm0 = MaxPooling2D(pool_size=pool_size, strides=pool_size, padding='valid', data_format='channels_first')(xm0)
-
-    xm1 = Conv2D(32, (1, 3), padding='same', name=mcSeq + "_pre_block model_conv01", kernel_initializer='glorot_normal', data_format='channels_first')(base)
-
-    xm = kb.concatenate([xm0, xm1], axis=0)
+    xm1 = Conv2D(32, (3, 1), padding='same', name=mcSeq + "_pre_block_model_conv01", kernel_initializer='glorot_normal', data_format='channels_first')(base)
+    xm = kb.concatenate([xm0, xm1], axis=2)
     # concat(xm0, xm1)
     return xm
 
@@ -110,31 +104,28 @@ def m_block(xm, mcSeq):
     print('进入m-block模块')
     base = xm
     base_xm = Conv2D(32, (1, 1), padding='same', name=mcSeq + "_m_block_conv1", kernel_initializer='glorot_normal', data_format='channels_first')(base)
-    xm0 = Conv2D(32, (3, 1), padding='same', name=mcSeq + "_m_block pool", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
-    xm1 = Conv2D(32, (1, 3), padding='same', name=mcSeq + "_m_block model_conv01", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
-    xm2 = Conv2D(32, (1, 1), padding='same', name=mcSeq + "_m_block model_conv02", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
-
-    xm = kb.concatenate([xm0, xm1], axis=0)
-    xm = kb.concatenate([xm, xm2], axis=0)
+    xm0 = Conv2D(32, (3, 1), padding='same', name=mcSeq + "_m_block_pool", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
+    xm1 = Conv2D(32, (3, 1), padding='same', name=mcSeq + "_m_block_model_conv01", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
+    xm2 = Conv2D(32, (1, 1), padding='same', name=mcSeq + "_m_block_model_conv02", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
+    xm = kb.concatenate([xm0, xm1], axis=2)
+    xm = kb.concatenate([xm, xm2], axis=2)
     # concat(xm0, xm1, xm2)
     return xm
 
+
 # m_block_pool模块
-def m_block_p(xm,mcSeq, pool_size):
+def m_block_p(xm, mcSeq, pool_size):
     print('进入m_block_pool模块')
     base = xm
     base_xm = Conv2D(32, (1, 1), padding='same', name=mcSeq + "_m_block_p_conv1", kernel_initializer='glorot_normal', data_format='channels_first')(base)
     xm0 = Conv2D(32, (3, 1), padding='same', name=mcSeq + "_pre_block_p_model_conv00", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
-    xm0 = MaxPooling2D(pool_size=pool_size, name=mcSeq+'_m_block_p_pool', kernel_initializer='glorot_normal', data_format='channels_first')(xm0)
-    xm1 = Conv2D(32, (1, 3), padding='same', name=mcSeq + "_pre_block_p_model_conv01", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
+    xm0 = MaxPooling2D(pool_size=pool_size, strides=pool_size, padding='valid', data_format='channels_first')(xm0)
+    xm1 = Conv2D(32, (3, 1), padding='same', name=mcSeq + "_pre_block_p_model_conv01", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
     xm2 = Conv2D(32, (1, 1), padding='same', name=mcSeq + "_pre_block_p_model_conv02", kernel_initializer='glorot_normal', data_format='channels_first')(base_xm)
-
-    xm = kb.concatenate([xm0, xm1], axis=0)
-    xm = kb.concatenate([xm, xm2], axis=0)
+    xm = kb.concatenate([xm0, xm1], axis=2)
+    xm = kb.concatenate([xm, xm2], axis=2)
     # concat(xm0, xm1, xm2)
     return xm
-
-
 
 
 # 建模
@@ -149,7 +140,6 @@ classes = ['AM',
            'GMSK',
            'APSK']
 
-
 in_shp = list(x_train.shape[1:])
 print(in_shp)
 
@@ -158,75 +148,79 @@ xm_input = Input(in_shp)
 xm = Reshape([1, 1024, 2], input_shape=in_shp)(xm_input)
 
 # conv
-xm = Conv2D(64, (3, 7), padding='same', name='conv0', kernel_initializer='glorot_normal', data_format='channels_first')(xm)
-
+xm = Conv2D(64, (3, 1), padding='same', name='conv0', kernel_initializer='glorot_normal', data_format='channels_first')(xm)
 
 # pool
-xm = MaxPooling2D(pool_size=(1, 3), name='pool0', kernel_initializer='glorot_normal', data_format='channels_first')(xm)
+xm = MaxPooling2D(pool_size=(2, 1), strides=(2, 1), padding='valid', data_format='channels_first')(xm)
 
 
 # keras.backend.concatenate
 
 # pre-block
-mcSeq = 'mc_net'    # 可以自行定义，每一层网络的名字。。。
-pool_size = (1, 3)
+mcSeq = 'mc_net01'  # 可以自行定义，每一层网络的名字。。。
+pool_size = (3, 1)
 xm = pre_block(xm, mcSeq, pool_size)
-xm_tmp1 = MaxPooling2D(pool_size=(1, 3), name='pool0', kernel_initializer='glorot_normal', data_format='channels_first')(xm)
+xm_tmp1 = MaxPooling2D(pool_size=(3, 1), strides=(3, 1), padding='valid', data_format='channels_first')(xm)
 
 # pool
-xm = MaxPooling2D(pool_size=(1, 3), name='pool1', kernel_initializer='glorot_normal', data_format='channels_first')(xm)
+xm = MaxPooling2D(pool_size=(3, 1), strides=(3, 1), padding='valid', data_format='channels_first')(xm)
 
 # M-block-p
+mcSeq = 'mc_net02'
 xm = m_block_p(xm, mcSeq, pool_size)
 
 # add
-xm = kb.concatenate([xm, xm_tmp1], axis=0)
+xm = kb.concatenate([xm, xm_tmp1], axis=2)
 xm_tmp2 = xm
 
 # M-block
+mcSeq = 'mc_net03'
 xm = m_block(xm, mcSeq)
 
 # add
-xm = kb.concatenate([xm, xm_tmp2], axis=0)
+xm = kb.concatenate([xm, xm_tmp2], axis=2)
 xm_tmp3 = xm
-xm_tmp3_pool = MaxPooling2D(pool_size=(1, 3), name='pool0', kernel_initializer='glorot_normal', data_format='channels_first')(xm_tmp3)
+xm_tmp3_pool = MaxPooling2D(pool_size=(3, 1), strides=(3, 1), padding='valid', data_format='channels_first')(xm_tmp3)
 
 # M-block-p
+mcSeq = 'mc_net04'
 xm = m_block_p(xm, mcSeq, pool_size)
 
-
 # add
-xm = kb.concatenate([xm, xm_tmp3_pool], axis=0)
+xm = kb.concatenate([xm, xm_tmp3_pool], axis=2)
 xm_tmp4 = xm
 # M-block
+mcSeq = 'mc_net05'
 xm = m_block(xm, mcSeq)
 
 # add
-xm = kb.concatenate([xm, xm_tmp4], axis=0)
+xm = kb.concatenate([xm, xm_tmp4], axis=2)
 xm_tmp5 = xm
-xm_tmp5_pool = MaxPooling2D(pool_size=(1, 3), name='pool0', kernel_initializer='glorot_normal', data_format='channels_first')(xm_tmp5)
+xm_tmp5_pool = MaxPooling2D(pool_size=(3, 1), strides=(3, 1), padding='valid', data_format='channels_first')(xm_tmp5)
+
 # M-block-p
+mcSeq = 'mc_net06'
 xm = m_block_p(xm, mcSeq, pool_size)
 # add
-xm = kb.concatenate([xm, xm_tmp5_pool], axis=0)
+xm = kb.concatenate([xm, xm_tmp5_pool], axis=2)
 xm_tmp6 = xm
 
 # M-block
+mcSeq = 'mc_net07'
 xm = m_block(xm, mcSeq)
 xm_tmp7 = xm
 
 # concat
-xm = kb.concatenate([xm, xm_tmp7], axis=0)
+xm = kb.concatenate([xm, xm_tmp7], axis=2)
 
 # avg-pool
-xm = AveragePooling2D(pool_size=(1, 3), name='pool0', kernel_initializer='glorot_normal', data_format='channels_first')(xm)
+xm = AveragePooling2D(pool_size=(3, 1), strides=(3, 1), padding='valid', data_format='channels_first')( xm)
 
 # dense fc
 xm = Dense(len(classes), kernel_initializer='glorot_normal', name="dense3")(xm)
 
 # softmax
 xm = Activation('softmax')(xm)
-
 
 model = model.Model(inputs=xm_input, outputs=xm)
 adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
@@ -238,7 +232,7 @@ print("训练开始")
 filepath = '/data/yqwan/ModelData/mcnet001.h5'
 history = model.fit(x_train, y_train,
                     batch_size=1024,
-                    epochs=200,
+                    epochs=10,
                     verbose=1,
                     validation_data=(x_test, y_test),
                     callbacks=[
@@ -278,9 +272,7 @@ plt.legend()
 # plt.savefig()
 plt.show()
 
-
 print("game over")
-
 
 
 
